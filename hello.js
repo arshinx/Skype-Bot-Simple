4ea40bb87818a8f04484c8ebb38ac51021e8b04e
Arshin Jain <arshin.jain@outlook.com>
2017-01-08 19:26:06 -0800

    Complete a ChatConnector Based Bot ready for integration
    #3

--- hello.js
+++ hello.js
@@ -3,25 +3,28 @@ var builder = require('botbuilder');    // Bots
 var restify = require('restify');       // REST APIs
 
 // Connect Bots - Create Chat Connector
-var connector = new builder.ChatConnector().listen();
+var connector = new builder.ChatConnector();
 
 // Bot Object - Create Bot
 var bot = new builder.UniversalBot(connector);
 
-// Bot Dialog
-// Note: Session object know the context and communicates with User
-bot.dialog('/', function(session){
-    // Retrieve User Input
-    var userMessage = session.message.text;
-    // Respond to User
-    session.send('You said: ' + userMessage);
-});
+// Bot Dialog based on Waterfall Approach: Moving linearly in functions
+bot.dialog('/',[
+    // First Function - Asks Question / Prompt
+    function(session) {
+        builder.Prompts.text(session, 'Please enter your name: ');
+    },
+    // Second Function - Responds with Prompted Answer
+    function(session, result) {
+        session.send('Hello, ' + result.response);
+    }
+]);
 
 // Create Server Object using Restify 
 var server = restify.createServer();
 
 // Set Port for Server 
-server.listen(process.env.port || process.env.PORT || 3978, function() {
+server.listen(process.env.port || process.env.PORT || 3000, function() {
     console.log('%s listening to %s', server.name, server.url);
 });
 
