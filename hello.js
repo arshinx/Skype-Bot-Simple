db835908953085f38c0b87101be0a66ac882af91
Arshin Jain <arshin.jain@outlook.com>
2017-01-08 18:12:56 -0800

    Bot Dialog

--- hello.js
+++ hello.js
@@ -7,3 +7,7 @@ var connector = new builder.ConsoleConnector().listen();
 // Bot Object
 var bot = new builder.UniversalBot(connector);
 
+// Bot Dialog
+bot.dialog('/', function(session){
+    session.send('Hello, Human!');
+});
